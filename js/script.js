a7ad262d2269415cd11f156edfa2471bad68404a
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-03 20:18:23 +0100

    update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.getElementById('gioca').addEventListener('click',
+function() {
+  // document.getElementById('sipariodx').classList.add('sipario-dx');
+  // document.getElementById('sipariosx').classList.add('sipario-sx');
+
+  // document.getElementById('helicopter').classList.add('animazione-sx');
+  // document.getElementById('eagle').classList.add('animazione-sx');
+  // document.getElementById('gunman1').classList.add('animazione-sx');
+  // document.getElementById('jet').classList.add('animazione-centro');
+  // document.getElementById('black-eagle').classList.add('animazione-centro');
+  // document.getElementById('gunman2').classList.add('animazione-centro');
+  // document.getElementById('air-balloon').classList.add('animazione-dx');
+  // document.getElementById('owl').classList.add('animazione-dx');
+  // document.getElementById('gunman3').classList.add('animazione-dx');
+}
+);
+document.getElementById('gunman2').addEventListener('click',
+function() {
+  document.getElementById('gunman2').classList.add('rotate');
+
+}
+);
